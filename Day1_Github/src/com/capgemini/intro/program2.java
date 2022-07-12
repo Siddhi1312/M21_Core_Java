9f819f27668e3752e01d6677101cf9175cca8946
siddh <siddh@DESKTOP-9ROJFDU>
2022-07-12 11:27:11 +0530

    2nd commit

---
+++ Day1_Github/src/com/capgemini/intro/program2.java
@@ -0,0 +1,10 @@
+package com.capgemini.intro;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day1_Github/src/com/cg/intro1/program3.java
@@ -0,0 +1,5 @@
+package com.cg.intro1;
+
+public class program3 {
+
+}
