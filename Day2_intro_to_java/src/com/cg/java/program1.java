664694db6278b34b7ebb4b1cccfed84b2a6df99c
siddh <siddh@DESKTOP-9ROJFDU>
2022-07-12 13:23:10 +0530

    commit 4

---
+++ Day2_intro_to_java/src/com/cg/java/program1.java
@@ -0,0 +1,19 @@
+package com.cg.java;
+import java.util.Scanner;
+public class program1 {
+
+	public static void main(String[] args) {
+		//int a=13;
+		Scanner s=new Scanner (System.in);
+		//scanner= class to get user input;s=object name and can be any
+		System.out.println("Enter value of a:");
+		double a= s.nextDouble();
+		
+		System.out.println(a);
+	
+       // System.out.println("Wecome to java coding program");
+		// s.close();-this should be used when there is error regarding object
+        
+	}
+
+}
