4c9532414d7a339c0b020e8782dfccb08ef6beb3
siddh <siddh@DESKTOP-9ROJFDU>
2022-07-17 15:55:32 +0530

    commit day5

--- Day1_Github/src/com/capgemini/intro/program2.java
+++ Day1_Github/src/com/capgemini/intro/program2.java
@@ -3,7 +3,8 @@ package com.capgemini.intro;
 public class program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a= 12;
+		System.out.println(a);
 
 	}
 
