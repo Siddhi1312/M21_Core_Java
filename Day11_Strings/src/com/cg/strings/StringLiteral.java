31a1558c5007447402af358b62d0151cb5756778
siddh <siddh@DESKTOP-9ROJFDU>
2022-07-23 16:42:30 +0530

    commit day11

---
+++ Day11_Strings/src/com/cg/strings/StringLiteral.java
@@ -0,0 +1,16 @@
+package com.cg.strings;
+
+import java.util.Scanner;
+
+public class StringLiteral {
+
+	public static void main(String[] args) {
+		//String str= "siddhi k";
+		//System.out.print(str);
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+		System.out.print(str1);
+
+	}
+
+}
---
+++ Day11_Strings/src/com/cg/strings/stringMethods.java
@@ -0,0 +1,28 @@
+package com.cg.strings;
+//length() method
+import java.util.Scanner;
+
+public class stringMethods {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str1=new String(s.nextLine());
+		String str=new String(s.nextLine());
+		//length function
+		System.out.println(str1.length());
+		//charAt() method
+		System.out.println(str1.charAt(1));
+		//toUpperCase() method
+		System.out.println(str1.toUpperCase());
+		//toLowerCase() method
+		System.out.println(str1.toLowerCase());
+		//there will be addition before string and merging after string
+		System.out.println(10+20+"hello"+"world"+10+20);
+		//concatenation operation
+		System.out.println(str.concat(str1));
+		
+		s.close();
+
+	}
+
+}
