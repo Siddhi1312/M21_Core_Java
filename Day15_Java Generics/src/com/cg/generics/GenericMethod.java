3a107f3820329ef56b2b4a845d32bb61b674a522
siddh <siddh@DESKTOP-9ROJFDU>
2022-07-28 19:45:35 +0530

    commit Day 15

---
+++ Day15_Java Generics/src/com/cg/generics/GenericMethod.java
@@ -0,0 +1,28 @@
+package com.cg.generics;
+
+public class GenericMethod {
+
+	public static void main(String[] args) {
+		Integer s[]= {13,14,15};
+		Character c[]= {'s','s','k'};
+		System.out.println("int array: ");
+		read (s);
+		System.out.println("char array: ");
+		read (c);
+	}
+      
+		//generic method
+		//E=element
+		public static <E> void read(E[] elements)
+		{
+			for(E elem:elements)
+			{
+				System.out.println(elem);
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+
