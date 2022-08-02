7d84888dbd8c62e6bcb88f1fbf3c0b1e3a45bb17
siddh <siddh@DESKTOP-9ROJFDU>
2022-08-02 19:51:39 +0530

    commit day18

---
+++ Day18_Date Time API/src/com/cg/datetime/DatetimeFormatExample.java
@@ -0,0 +1,16 @@
+package com.cg.datetime;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DatetimeFormatExample {
+
+	public static void main(String[] args) {
+		String datetime ="2022-04-27 18:15";
+		DateTimeFormatter df=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		LocalDateTime dt=LocalDateTime.parse(datetime,df);
+		System.out.println(dt);
+
+	}
+
+}
