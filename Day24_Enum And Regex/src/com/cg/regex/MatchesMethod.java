ea911537adfd9b01af47c22786bba46013dd14f8
siddh <siddh@DESKTOP-9ROJFDU>
2022-08-07 20:09:48 +0530

    commit day24

---
+++ Day24_Enum And Regex/src/com/cg/regex/MatchesMethod.java
@@ -0,0 +1,20 @@
+package com.cg.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class MatchesMethod {
+
+	public static void main(String[] args) {
+		String str="sid k";
+		Scanner s=new Scanner(System.in);
+		String input=s.nextLine();
+		boolean result=Pattern.matches(str,input);
+		System.out.println(result);
+		s.close();
+		
+		
+
+	}
+
+}
